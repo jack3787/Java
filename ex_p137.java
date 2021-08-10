76c6ba218d65c61060f518b50915934590bf7f90
JAE <jack3787@naver.com>
2021-08-11 01:37:57 +0900

    예제 p.137

---
+++ ex_p137.java
@@ -0,0 +1,16 @@
+public class ex_p137 {
+    public static void main(String args[]){
+//        int i []= new int[5];
+//        int i2 [];
+//        int i3 [] = {};
+//        String users = "egoing, jinhuck, youbin";
+        String [] users = new String [3]; //서랍 3칸 느낌
+        users[0] = "egoing";
+        users[1] = "jinhuck";
+        users[2] = "youbin";
+
+        System.out.println(users[1]);
+
+
+    }
+}
