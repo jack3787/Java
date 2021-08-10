6406f25a01564f5c8a66db6062b6fe075ccbda5f
JAE <jack3787@naver.com>
2021-08-10 20:10:54 +0900

    예제 p.127

---
+++ ex_p127.java
@@ -0,0 +1,14 @@
+public class ex_p127 {
+    public static void main(String args[]){
+        System.out.print("1부터 100까지 3의 배수의 합>>");
+        int n;
+        int sum = 0;
+
+        for(int i=1; (3*i)<100; i++)
+        {
+            sum = sum + 3*i;
+        }
+ssssss
+        System.out.print(sum);
+    }
+}
